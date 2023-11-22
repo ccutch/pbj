7c197e6946bc12b5eb03de641f3048d23173e874
Connor McCutcheon <connormccutcheon95@gmail.com>
2023-11-22 19:49:36 +0000

    AI generated code

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,46 +1,19 @@
-package main
+o
+// Events (SQLITE CRUD)
+app.Events(func(events *Events) {
+    events.POST("/create", func (c echo.Context) error {
+        // Your handler for create event.
+    })
 
-import (
-	"log"
+    events.GET("/read", func (c echo.Context) error {
+        // Your handler for read event.
+    })
 
-	. "github.com/ccutch/pb-j-stack"
+    events.PUT("/update", func (c echo.Context) error {
+        // Your handler for update event.
+    })
 
-	"github.com/labstack/echo/v5"
-)
-
-func main() {
-
-	app := NewApp(
-		WithStylesheet("https://cdn.jsdelivr.net/npm/daisyui@3.9.4/dist/full.css"),
-		WithScript("https://cdn.tailwindcss.com"),
-	)
-
-	// Pages (HTTP GET)
-	app.Static(HomePage(WithTemplate("home")))
-	app.Static(NewPage("about"))
-	app.Static(NewPage("path-to-profit"))
-	app.Serve(NewPage("hello/:name"), func(c echo.Context) (any, error) {
-		// Construct a struct to say hello
-		return struct{ Name string }{
-			Name: c.PathParam("name"),
-		}, nil
-	})
-
-	// Callbacks (HTTP POST)
-	app.On("hello-again", func(c *HandlerContext) error {
-		return c.Refresh()
-	})
-
-	app.On("hello-another", func(c *HandlerContext) error {
-		return c.Redirect("hello/" + c.FormValue("name"))
-	})
-
-	// Events (SQLITE CRUD)
-	// app.Events(func(events *Events) {
-	// TODO: REST triggered events
-	// })
-
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
-}
+    events.DELETE("/delete", func (c echo.Context) error {
+        // Your handler for delete event.
+    })
+})
