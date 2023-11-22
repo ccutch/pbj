df558f55c86785f6b96ecacf6b95c5e1ca6022f5
Connor McCutcheon <connormccutcheon95@gmail.com>
2023-11-22 09:47:55 +0000

    AI generated code

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,46 +1,24 @@
-package main
-
-import (
-	"log"
-
-	. "github.com/ccutch/pb-j-stack"
-
-	"github.com/labstack/echo/v5"
-)
-
-func main() {
-
-	app := NewApp(
-		WithStylesheet("https://cdn.jsdelivr.net/npm/daisyui@3.9.4/dist/full.css"),
-		WithScript("https://cdn.tailwindcss.com"),
-	)
-
-	// Pages (HTTP GET)
-	app.Static(HomePage(WithTemplate("home")))
-	app.Static(NewPage("about"))
-	app.Static(NewPage("path-to-profit"))
-	app.Serve(NewPage("hello/:name"), func(c echo.Context) (any, error) {
-		// Construct a struct to say hello
-		return struct{ Name string }{
-			Name: c.PathParam("name"),
-		}, nil
+o
+// Events (SQLITE CRUD)
+app.Events(func(events *Events) {
+	// REST triggered events
+	events.OnCreate("resource", func(req *DbContext, res *Resource) error {
+		// TODO: Handle Create event for 'resource'.
+		return nil
 	})
 
-	// Callbacks (HTTP POST)
-	app.On("hello-again", func(c *HandlerContext) error {
-		return c.Refresh()
+	events.OnRead("resource", func(req *DbContext) (*Resource, error) {
+		// TODO: Handle Read 'resource' request event. 
+		return nil, nil
 	})
 
-	app.On("hello-another", func(c *HandlerContext) error {
-		return c.Redirect("hello/" + c.FormValue("name"))
+	events.OnUpdate("resource", func(req *DbContext, res *Resource) error {
+		// TODO: Handle Update event for 'resource'.
+		return nil
 	})
 
-	// Events (SQLITE CRUD)
-	// app.Events(func(events *Events) {
-	// TODO: REST triggered events
-	// })
-
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
-}
+	events.OnDelete("resource", func(req *DbContext) error {
+		// TODO: Handle Delete 'resource' event.
+		return nil
+	})
+})
