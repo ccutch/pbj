b9f6400f0d445a23c9459490e19198de0adcccc7
Connor McCutcheon <connormccutcheon95@gmail.com>
2023-11-23 06:15:50 +0000

    AI generated code

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -1,46 +1,26 @@
-package main
-
-import (
-	"log"
-
-	. "github.com/ccutch/pb-j-stack"
-
-	"github.com/labstack/echo/v5"
-)
-
-func main() {
-
-	app := NewApp(
-		WithStylesheet("https://cdn.jsdelivr.net/npm/daisyui@3.9.4/dist/full.css"),
-		WithScript("https://cdn.tailwindcss.com"),
-	)
-
-	// Pages (HTTP GET)
-	app.Static(HomePage(WithTemplate("home")))
-	app.Static(NewPage("about"))
-	app.Static(NewPage("path-to-profit"))
-	app.Serve(NewPage("hello/:name"), func(c echo.Context) (any, error) {
-		// Construct a struct to say hello
-		return struct{ Name string }{
-			Name: c.PathParam("name"),
-		}, nil
+o
+app.Events(func(events *Events) {
+	// Create User
+	events.On("create-user", func(c *HandlerContext) error {
+		// Implement logic for creating a user here
+		// This could involve getting incoming data from c, validating it, and storing it in the database
 	})
 
-	// Callbacks (HTTP POST)
-	app.On("hello-again", func(c *HandlerContext) error {
-		return c.Refresh()
+	// Read User
+	events.On("read-user", func(c *HandlerContext) error {
+		// Implement logic for reading a user here
+		// This could involve getting the user ID from c, retrieving that user from the database, and returning it
 	})
 
-	app.On("hello-another", func(c *HandlerContext) error {
-		return c.Redirect("hello/" + c.FormValue("name"))
+	// Update User
+	events.On("update-user", func(c *HandlerContext) error {
+		// Implement logic for updating a user here
+		// This could involve getting incoming data and the user ID from c, updating the user in the database, and returning the updated user
 	})
 
-	// Events (SQLITE CRUD)
-	// app.Events(func(events *Events) {
-	// TODO: REST triggered events
-	// })
-
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
-}
+	// Delete User
+	events.On("delete-user", func(c *HandlerContext) error {
+		// Implement logic for deleting a user here
+		// This could involve getting the user ID from c and deleting the user from the database
+	})
+})
